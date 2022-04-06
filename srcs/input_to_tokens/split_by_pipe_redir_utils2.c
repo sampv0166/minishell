f8162fb8202c10640139cf3cba16545bf96e598d
sampv0166@gmail.com <sampv0166@gmail.com>
2022-04-06 11:42:39 +0400

    restored file from trash

---
+++ srcs/input_to_tokens/split_by_pipe_redir_utils2.c
@@ -0,0 +1,58 @@
+#include "../../includes/mini_shell.h"
+void create_out_redirection_character(char **arr, t_split *split_info, char **tokens)
+{
+    int len;
+    len = 0;
+
+    if (arr[split_info->i][split_info->j] == '>' && arr[split_info->i][split_info->j + 1] == '>')
+    {
+        while(arr[split_info->i][split_info->j] == '>')
+        {
+            split_info->j++;
+            len++;
+        }
+        tokens[split_info->k++] = ft_substr(arr[split_info->i], (split_info->j) - len, len); 
+    }
+    else if(arr[split_info->i][split_info->j] == '>' && arr[split_info->i][split_info->j + 1] != '>')
+    {
+        tokens[split_info->k++] = ft_strdup(">");
+        split_info->j++;   
+    }
+}
+
+void create_in_redirection_character(char **arr, t_split *split_info, char **tokens)
+{
+    int len;
+    len = 0;
+
+    if (arr[split_info->i][split_info->j] == '<' && arr[split_info->i][split_info->j + 1] == '<')
+    {
+        while(arr[split_info->i][split_info->j] == '<')
+        {
+            split_info->j++;
+            len++;
+        }
+        tokens[split_info->k++] = ft_substr(arr[split_info->i], (split_info->j) - len, len);
+    }
+    if (arr[split_info->i][split_info->j] == '<' && arr[split_info->i][split_info->j + 1] != '<')
+    {
+        tokens[split_info->k++] = ft_strdup("<");
+        split_info->j++;   
+    }
+}
+
+void create_pipe_redirection_character(char **arr, t_split *split_info, char **tokens)
+{
+    int len;
+    len = 0;
+
+    if (arr[split_info->i][split_info->j] == '|')
+    {
+        while(arr[split_info->i][split_info->j] == '|')
+        {
+            split_info->j++;
+            len++;
+        }
+        tokens[split_info->k++] = ft_substr(arr[split_info->i], (split_info->j) - len, len);   
+    }
+}
